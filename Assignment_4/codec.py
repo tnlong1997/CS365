e952e49f57499105a8718256d3afbe65be139da7
tnlong1997 <tnlong1997@gmail.com>
2018-02-14 13:49:45 -0500

    Codec

--- Assignment_4/codec.py
+++ Assignment_4/codec.py
@@ -1,3 +1,6 @@
+import struct
+
+
 UTF_8_BASE = 6
 
 
@@ -16,45 +19,59 @@ def encode(codepoint):
     numberOfBits = count_number_of_bits(codepoint)
 
     if (numberOfBits > 16):
-        codepoint = (30 << numberOfBits) | codepoint
         numberOfBytes = 4
     elif (numberOfBits > 11):
-        codepoint = (14 << numberOfBits) | codepoint
         numberOfBytes = 3
     else:
-        codepoint = (6 << numberOfBits) | codepoint
         numberOfBytes = 2
 
-    res = 0
-    baseByte = 0b111111
+    res = None
+    baseByte = 63
     numberOfRightBits = 0
     for i in range(0, numberOfBytes - 1):
         bits = (codepoint & baseByte) >> numberOfRightBits
         bits = (2 << UTF_8_BASE) | bits
         codepoint = codepoint >> UTF_8_BASE
-        res = (res << (8 * i)) | bits
+        if res is None:
+            res = struct.pack('>B', bits)
+        else:
+            res = struct.pack('>B', bits) + res
 
     if (numberOfBits > 16):
-        codepoint = (30 << numberOfBits) | codepoint
-        numberOfBytes = 4
+        res = struct.pack('>B', (30 << 3) | codepoint) + res
     elif (numberOfBits > 11):
-        codepoint = (14 << numberOfBits) | codepoint
-        numberOfBytes = 3
+        res = struct.pack('>B', (14 << 4) | codepoint) + res
     else:
-        codepoint = (6 << numberOfBits) | codepoint
-        numberOfBytes = 2
-    print(res)
+        res = struct.pack('>B', (6 << 5) | codepoint) + res
+
     return res
 
 
-
-
 def decode(bytes_object):
-    return ord(bytes_object.decode())
+    if len(bytes_object) == 1:
+        return bytes_object[0]
+
+    temp = 0
+    res = 0
+    base = 0
+    for i in range(len(bytes_object) - 1, 0, -1):
+        temp = bytes_object[i] & 63
+        res = res | (temp << (base * 6))
+        base += 1
+
+    if len(bytes_object) == 4:
+        res = res | ((bytes_object[0] & 7) << 18)
+    elif len(bytes_object) == 3:
+        res = res | ((bytes_object[0] & 15) << 12)
+    else:
+        res = res | ((bytes_object[0] & 31) << 6)
+
+    return res
 
 
 def main():
     pass
+
 
 if __name__ == '__main__':
     main()
