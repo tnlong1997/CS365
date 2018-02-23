73d31cfb7e59ffd6112b3d129f4afde8d7829636
tnlong1997 <tnlong1997@gmail.com>
2018-02-22 23:43:03 -0500

    Working on parsing

--- Assignment_5/jpeg_exif.py
+++ Assignment_5/jpeg_exif.py
@@ -18,10 +18,34 @@ def find_jfif(f, max_length=None):
 
     # then return a possibly-empty sequence of pairs
 
-    # here's an example that just returns the start and end of the file without parsing
-    chunk = f.read()
-    last_byte = len(chunk)
-    return [(0, last_byte)]
+    # here's an example that just returns the start and end of the file
+    # without parsing
+    res = []
+    index = 0
+    soiList = []
+    eoiList = []
+    byte = f.read(1)
+    while len(byte) != 0:
+        if (byte == b'\xff'):
+            byte = f.read(1)
+            index += 1
+            if byte == b'\xd8':
+                soiList = soiList + [index - 1]
+            elif byte == b'\xd9':
+                eoiList = eoiList + [index]
+        else:
+            index += 1
+            byte = f.read(1)
+
+    for soi in soiList:
+        for eoi in eoiList:
+            if (max_length is not None):
+                if (eoi - soi + 1 <= max_length) and (soi < eoi):
+                    res = res + [(soi, eoi)]
+            else:
+                if (soi < eoi):
+                    res = res + [(soi, eoi)]
+    return res
 
 
 def parse_exif(f):
@@ -32,5 +56,4 @@ def parse_exif(f):
     # Don't hardcode the answer! Return your computed dictionary.
     return {'Make':['Apple']}
 
-
-print(find_jfif(open('minimal.jpg', 'rb')))
+# print(find_jfif(open('minimal.jpg', 'rb')))
