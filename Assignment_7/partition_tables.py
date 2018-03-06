ae840a28c0cc7d3d1dd2e563cc9d5df300eab955
tnlong1997 <tnlong1997@gmail.com>
2018-03-06 16:58:01 -0500

    MBR done - working on GPT

--- Assignment_7/partition_tables.py
+++ Assignment_7/partition_tables.py
@@ -2,9 +2,30 @@ import struct
 import uuid
 
 
+NULL_HEX = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+
+
 def parse_mbr(mbr_bytes):
-    return []
+    res = []
+    for i in range(0, 4):
+        entry = mbr_bytes[446 + i*16:446 + i*16 + 16]
+        entry_dict = {}
+        if (entry != NULL_HEX):
+            t = entry[4]
+            start = struct.unpack('<I', entry[8:12])[0]
+            end = start + struct.unpack('<I', entry[12:16])[0] - 1
+            entry_dict['type'] = hex(t)
+            entry_dict['start'] = start
+            entry_dict['end'] = end
+            entry_dict['number'] = i
+            res = res + [entry_dict]
+    return res
 
 
 def parse_gpt(gpt_file, sector_size=512):
     return []
+
+
+# with open('usb-mbr.dd', 'rb') as f:
+#     data = f.read(512)
+# parse_mbr(data)
