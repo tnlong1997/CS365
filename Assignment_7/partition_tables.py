9ecdea0aa1c12fa1d94777cd108d33d20b345e90
tnlong1997 <tnlong1997@gmail.com>
2018-03-07 21:35:56 -0500

    Assignment 7

--- Assignment_7/partition_tables.py
+++ Assignment_7/partition_tables.py
@@ -23,9 +23,35 @@ def parse_mbr(mbr_bytes):
 
 
 def parse_gpt(gpt_file, sector_size=512):
-    return []
+    res = []
+    gpt_file.read(sector_size)
+    header = gpt_file.read(sector_size)
+    partion_index = struct.unpack('<Q', header[72:80])[0]
+    numberOfEntries = struct.unpack('<I', header[80:84])[0]
+    entrySize = struct.unpack('<I', header[84:88])[0]
+    for i in range(2, partion_index):
+        gpt_file.read(sector_size)
+    for i in range(0, numberOfEntries):
+        entry = gpt_file.read(entrySize)
+        entry_dict = {}
+        if (entry != (entrySize * b'\x00')):
+            t = entry[0:16]
+            start = struct.unpack('<Q', entry[32:40])[0]
+            end = struct.unpack('<Q', entry[40:48])[0]
+            entry_dict['start'] = start
+            entry_dict['end'] = end
+            entry_dict['type'] = uuid.UUID(bytes_le=t)
+            entry_dict['number'] = i
+            name = ''
+            for i in range(56, 128, 2):
+                if (entry[i] != 0):
+                    name = name + chr(entry[i])
+                else:
+                    break
+            entry_dict['name'] = name
+            res = res + [entry_dict]
+    return res
 
 
-# with open('usb-mbr.dd', 'rb') as f:
-#     data = f.read(512)
-# parse_mbr(data)
+with open('disk-image.dd', 'rb') as f:
+    parse_gpt(f, 512)
