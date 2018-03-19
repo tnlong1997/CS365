4e2ed5319a103d5f6c4654506d43a3be991fc2e3
tnlong1997 <tnlong1997@gmail.com>
2018-03-19 15:42:14 -0400

    Assignment 8

---
+++ Assignment_8/fsstat_fat16.py
@@ -0,0 +1,12 @@
+import struct
+
+
+def fsstat_fat16(fat16_file, sector_size=512, offset=0):
+    result = ['FILE SYSTEM INFORMATION',
+              '--------------------------------------------',
+              'File System Type: FAT16',
+              '']
+
+    # then do a few things, .append()ing to result as needed
+
+    return result
